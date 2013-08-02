70fd005fe750b5e9289c4ae917d86c30c9ce6e30
Erik Olsson <meriko@hotmail.com>
2013-08-02 16:47:03 +0200

    Added radio, added formats and categories for channels, add live support for radio. Almost complete now...

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,11 +5,12 @@ import config
 TITLE  = "BBC iPlayer"
 PREFIX = "/video/iplayer"
 
-BBC_URL           = "http://www.bbc.co.uk"
-BBC_FEED_URL      = "http://feeds.bbc.co.uk"
-BBC_LIVE_TV_URL   = "%s/iplayer/tv/%%s/watchlive" % BBC_URL
-BBC_SEARCH_URL    = "%s/iplayer/search?q=%%s&page=%%s" % BBC_URL
-BBC_SEARCH_TV_URL = BBC_SEARCH_URL + "&filter=tv"
+BBC_URL              = "http://www.bbc.co.uk"
+BBC_FEED_URL         = "http://feeds.bbc.co.uk"
+BBC_LIVE_TV_URL      = "%s/iplayer/tv/%%s/watchlive" % BBC_URL
+BBC_SEARCH_URL       = "%s/iplayer/search?q=%%s&page=%%s" % BBC_URL
+BBC_SEARCH_TV_URL    = BBC_SEARCH_URL + "&filter=tv"
+BBC_SEARCH_RADIO_URL = BBC_SEARCH_URL + "&filter=radio"
 
 RE_SEARCH      = Regex('episodeRegistry\\.addData\\((.*?)\\);', Regex.IGNORECASE | Regex.DOTALL)
 RE_ORDER       = Regex('class="cta-add-to-favourites" href="pid-(.*?)"')
@@ -47,40 +48,54 @@ def Start():
 def MainMenu():
     oc = ObjectContainer()
     
-    oc.add(DirectoryObject(key=Callback(Highlights), title="Highlights"))
-    oc.add(DirectoryObject(key=Callback(Popular), title="Most Popular"))
-    oc.add(DirectoryObject(key=Callback(Channels), title="Channels"))
+    oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="TV Highlights", url=BBC_FEED_URL + "/iplayer/highlights/tv"), title="TV Highlights"))
+    oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Radio Highlights", url=BBC_FEED_URL + "/iplayer/highlights/radio"), title="Radio Highlights"))
+    oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Most Popular TV", url=BBC_FEED_URL + "/iplayer/popular/tv"), title="Most Popular TV"))
+    oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Most Popular Radio", url=BBC_FEED_URL + "/iplayer/popular/radio"), title="Most Popular Radio"))
+    oc.add(DirectoryObject(key=Callback(TVChannels), title="TV Channels"))
+    oc.add(DirectoryObject(key=Callback(RadioStations), title="Radio Stations"))
     oc.add(DirectoryObject(key=Callback(Categories), title="Categories"))
     oc.add(DirectoryObject(key=Callback(Formats), title="Formats"))
     oc.add(DirectoryObject(key=Callback(AToZ), title="A-Z"))
-    oc.add(InputDirectoryObject(key = Callback(Search), title="Search", prompt="Search")) 
+    oc.add(InputDirectoryObject(key = Callback(Search, search_url=BBC_SEARCH_TV_URL), title="Search TV", prompt="Search TV"))
+    oc.add(InputDirectoryObject(key = Callback(Search, search_url=BBC_SEARCH_RADIO_URL), title="Search Radio", prompt="Search Radio"))
 
     return oc
 
 ##########################################################################################
-@route(PREFIX + "/Channels")
-def Channels():
-    oc = ObjectContainer(title2="Channels")
+@route(PREFIX + "/TVChannels")
+def TVChannels():
+    oc = ObjectContainer(title2="TV Channels")
     for channel_id in content.ordered_tv_channels:
         channel = content.tv_channels[channel_id] 
         thumb = Resource.ContentsOfURLWithFallback(url=channel.thumb_url, fallback=ICON_DEFAULT)
-        oc.add(DirectoryObject(key=Callback(Channel, channel_id=channel_id), title=channel.title, thumb=thumb))
+        oc.add(DirectoryObject(key=Callback(Channel, tv=True, channel_id=channel_id), title=channel.title, thumb=thumb))
+    return oc
+    
+##########################################################################################
+@route(PREFIX + "/RadioStations")
+def RadioStations():
+    oc = ObjectContainer(title2="Radio Stations")
+    for channel_id in content.ordered_radio_channels:
+        channel = content.radio_channels[channel_id] 
+        thumb = Resource.ContentsOfURLWithFallback(url=channel.thumb_url, fallback=ICON_DEFAULT)
+        oc.add(DirectoryObject(key=Callback(Channel, tv=False, channel_id=channel_id), title=channel.title, thumb=thumb))
     return oc
 
 ##########################################################################################
 @route(PREFIX + "/Categories")
-def Categories():
+def Categories(channel_id=None, thumb=None):
     oc = ObjectContainer(title2="Categories")
     for category in content.categories:
-        oc.add(DirectoryObject(key=Callback(Category, category_id=category.id), title=category.title))
+        oc.add(DirectoryObject(key=Callback(Category, category_id=category.id, channel_id=channel_id), title=category.title, thumb=thumb))
     return oc
     
 ##########################################################################################
 @route(PREFIX + "/Formats")
-def Formats():
+def Formats(channel_id=None, thumb=None):
     oc = ObjectContainer(title2="Formats")
     for format in content.formats:
-        oc.add(DirectoryObject(key=Callback(Format, format_id=format.id), title=format.title))
+        oc.add(DirectoryObject(key=Callback(VideosFromJSONEpisodeList, title=format.title, url=format.url(channel_id)), title=format.title, thumb=thumb))
     return oc
     
 ##########################################################################################
@@ -89,116 +104,69 @@ def AToZ():
     oc = ObjectContainer(title2="A - Z")
     for code in range(ord('a'), ord('z') + 1):
         letter = chr(code)
-        oc.add(DirectoryObject(key=Callback(RSSListContainer, title=letter.upper(), url=BBC_FEED_URL + "/iplayer/atoz/%s/list" % letter), title=letter.upper()))
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title=letter.upper(), url=BBC_FEED_URL + "/iplayer/atoz/%s/list" % letter), title=letter.upper()))
     return oc
 
 ##########################################################################################
 @route(PREFIX + "/Category")
-def Category(category_id):
+def Category(category_id, channel_id=None):
     category = content.category[category_id]
     oc = ObjectContainer(title1=category.title)
-    oc.add(DirectoryObject(key=Callback(CategoryHighlights, category_id=category_id), title="Highlights"))
-    oc.add(DirectoryObject(key=Callback(CategoryPopular, category_id=category_id), title="Most Popular"))
-    for subcategory in category.subcategories:
-        oc.add(DirectoryObject(key=Callback(Subcategory, category_id=category_id, subcategory_id=subcategory.id), title=subcategory.title))
-    return oc
     
-##########################################################################################
-@route(PREFIX + "/Format")
-def Format(format_id):
-    format = content.format[format_id]
-    return JSONEpisodeListContainer(title=format.title, url=format.url())
+    if channel_id:
+        return VideosFromJSONEpisodeList(title=category.title, url=category.genre_url(channel_id + "/programmes"))
+    else:
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="%s TV Highlights" % category.title, url=category.highlights_url(tv=True)), title="TV Highlights"))
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="%s Radio Highlights" % category.title, url=category.highlights_url(tv=False)), title="Radio Highlights"))
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Popular %s TV" % category.title, url=category.popular_url(tv=True)), title="Most Popular TV"))
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Popular %s Radio" % category.title, url=category.popular_url(tv=False)), title="Most Popular Radio"))
+        oc.add(DirectoryObject(key=Callback(VideosFromJSONEpisodeList, title="All programmes", url=category.genre_url(channel_id="programmes")), title="All programmes"))
+        oc.add(DirectoryObject(key=Callback(VideosFromJSONEpisodeList, title="TV programmes", url=category.genre_url(channel_id="tv/programmes")), title="TV programmes"))
+        oc.add(DirectoryObject(key=Callback(VideosFromJSONEpisodeList, title="Radio programmes", url=category.genre_url(channel_id="radio/programmes")), title="Radio programmes"))
+        for subcategory in category.subcategories:
+            oc.add(DirectoryObject(key=Callback(VideosFromRSS, title=subcategory.title, url=category.subcategory_url(subcategory.id), sort=True), title=subcategory.title))
+        
+    return oc
 
 ##########################################################################################
-@route(PREFIX + "/CategoryHighlights")
-def CategoryHighlights(category_id):
-    category = content.category[category_id]
-    return RSSListContainer(title="%s Highlights" % category.title, url=category.highlights_url())
-
-##########################################################################################
-@route(PREFIX + "/CategoryPopular")
-def CategoryPopular(category_id):
-    category = content.category[category_id]
-    return RSSListContainer(title="Popular %s" % category.title, url=category.popular_url())
-
-##########################################################################################
-@route(PREFIX + "/Subcategory")
-def Subcategory(category_id, subcategory_id):
-    category = content.category[category_id]
-    return RSSListContainer(title=category.title, url=category.subcategory_url(subcategory_id), sort=True)
-
-##########################################################################################
-@route(PREFIX + "/Highlights")
-def Highlights():
-    url = BBC_FEED_URL + "/iplayer/highlights/tv" 
-    return RSSListContainer(title="Highlights", url=url)
-
-##########################################################################################
-@route(PREFIX + "/Popular")
-def Popular():
-    url = BBC_FEED_URL + "/iplayer/popular/tv"
-    return RSSListContainer(title="Most Popular", url=url)
-
-##########################################################################################
-@route(PREFIX + "/Channel")
-def Channel(channel_id):
-    channel = content.tv_channels[channel_id]
+@route(PREFIX + "/Channel", tv = bool)
+def Channel(tv, channel_id):
+    if tv:
+        channel = content.tv_channels[channel_id]
+    else:
+        channel = content.radio_channels[channel_id]
+   
     oc = ObjectContainer(title1=channel.title)
     thumb = Resource.ContentsOfURLWithFallback(url=channel.thumb_url, fallback=ICON_DEFAULT)
 
-    if channel.can_watch_live():
-        oc.add(VideoClipObject(url = channel.live_url(), title = "Watch Live", thumb = thumb))
+    if channel.has_live_broadcasts():
+        oc.add(VideoClipObject(url = channel.live_url(), title = "Live", thumb = thumb))
 
     if channel.has_highlights():
-        oc.add(DirectoryObject(key=Callback(ChannelHighlights, channel_id=channel_id), title="Highlights", thumb=thumb))
-        oc.add(DirectoryObject(key=Callback(ChannelPopular, channel_id=channel_id), title="Most Popular", thumb=thumb))
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Highlights", url=channel.highlights_url()), title="Highlights", thumb=thumb))
+        oc.add(DirectoryObject(key=Callback(VideosFromRSS, title="Most Popular", url=channel.popular_url()), title="Most Popular", thumb=thumb))
 
-    # FIXME: do we need categories/formats per channel?
-    #dir.Append(Function(DirectoryItem(AddCategories, title = "Categories", subtitle = sender.itemTitle, thumb = thumb), thumb = thumb, channel_id = json_channel_id, thumb_url = thumb_url, player_url = player_url))
-    #dir.Append(Function(DirectoryItem(AddFormats, title = "Formats", subtitle = sender.itemTitle, thumb = thumb), thumb = thumb, channel_id = json_channel_id, thumb_url = thumb_url, player_url = player_url))
+    oc.add(DirectoryObject(key=Callback(Categories, channel_id=channel_id, thumb=thumb), title="Categories", thumb=thumb))
 
-    # Add the last week's worth of schedules
-    oc.add(DirectoryObject(key=Callback(Schedule, for_when="today", channel_id=channel_id), title="Today", thumb=thumb))
-    oc.add(DirectoryObject(key=Callback(Schedule, for_when="yesterday", channel_id=channel_id), title="Yesterday", thumb=thumb))
-    now = datetime.today()
-    for i in range (2, 7):
-        date = now - Datetime.Delta(days=i)
-        oc.add(DirectoryObject(key=Callback(ScheduleForDay, channel_id=channel_id, year=date.year, month=date.month, day=date.day), title=DAYS[date.weekday()], thumb=thumb))
+    if channel.has_live_broadcasts():
+        # Add the last week's worth of schedules
+        oc.add(DirectoryObject(key=Callback(VideosFromJSONScheduleList, title=channel.title, url=channel.schedule_url + "today.json"), title="Today", thumb=thumb))
+        oc.add(DirectoryObject(key=Callback(VideosFromJSONScheduleList, title=channel.title, url=channel.schedule_url + "yesterday"), title="Yesterday", thumb=thumb))
+        now = datetime.today()
+        for i in range (2, 7):
+            date = now - Datetime.Delta(days=i)
+            oc.add(DirectoryObject(key=Callback(VideosFromJSONScheduleList, title=channel.title, url="%s/%s/%s/%s.json" % (channel.schedule_url, date.year, date.month, date.day)), title=DAYS[date.weekday()], thumb=thumb))
+
+    oc.add(DirectoryObject(key=Callback(Formats, channel_id=channel_id, thumb=thumb), title="Formats", thumb=thumb))
 
     return oc
 
 ##########################################################################################
-@route(PREFIX + "/ChannelPopular")
-def ChannelPopular(channel_id):
-    channel = content.tv_channels[channel_id]
-    return RSSListContainer(title="Most Popular", url=channel.popular_url())
-
-##########################################################################################
-@route(PREFIX + "/ChannelHighlights")
-def ChannelHighlights(channel_id):
-    channel = content.tv_channels[channel_id]
-    return RSSListContainer(title="Highlights", url=channel.highlights_url())
-
-##########################################################################################
-@route(PREFIX + "/ScheduleForDay")
-def ScheduleForDay(channel_id, year, month, day):
-    channel = content.tv_channels[channel_id]
-    url = "%s/%s/%s/%s.json" % (channel.schedule_url, year, month, day)
-    return JSONScheduleListContainer(title=channel.title, url=url)
-
-##########################################################################################
-@route(PREFIX + "/Schedule")
-def Schedule(channel_id, for_when):
-    channel = content.tv_channels[channel_id]
-    url = channel.schedule_url + for_when + ".json"
-    return JSONScheduleListContainer(title=channel.title, url=url)
-
-##########################################################################################
-@route(PREFIX + "/RSSListContainer", offset = int)
-def RSSListContainer(title="", url=None, sort=False, offset=0):
+@route(PREFIX + "/VideosFromRSS", offset = int)
+def VideosFromRSS(title="", url=None, sort=False, offset=0):
     thumb_url = "http://node2.bbcimg.co.uk/iplayer/images/episode/%s_640_360.jpg"
 
-    feed = RSS.FeedFromString(url)
+    feed = RSS.FeedFromURL(url)
     if feed is None: return
 
     oc = ObjectContainer(title1=title)
@@ -232,13 +200,11 @@ def RSSListContainer(title="", url=None, sort=False, offset=0):
             nextPage = (offset / MAX_RSS_ITEMS_PER_PAGE) + 2
             lastPage = (totalEntries / MAX_RSS_ITEMS_PER_PAGE) + 1
             titleNextPage = "Next page (" + str(nextPage) + "/" + str(lastPage) + ")..."
-            oc.add(NextPageObject(key=Callback(RSSListContainer, url=url, sort=sort, offset=counter), title=titleNextPage))
+            oc.add(NextPageObject(key=Callback(VideosFromRSS, url=url, sort=sort, offset=counter), title=titleNextPage))
             return oc
 
     if len(oc) == 0:
-        oc.header  = title
-        oc.message = "No programmes found."
-        return oc
+        return NoProgrammesFound(oc, title)
 
     if sort:
         oc.objects.sort(key=lambda obj: obj.title)
@@ -246,14 +212,17 @@ def RSSListContainer(title="", url=None, sort=False, offset=0):
     return oc
 
 ##########################################################################################
-def JSONEpisodeListContainer(title, url):
+@route(PREFIX + "/VideosFromJSONEpisodeList", hd = bool)
+def VideosFromJSONEpisodeList(title, url):
 
     # this function generates the category lists and format lists from a JSON feed
-
-    jsonObj = JSON.ObjectFromURL(url)
-    if jsonObj is None: return
-
     oc = ObjectContainer(title1 = title)
+
+    try:
+        jsonObj = JSON.ObjectFromURL(url)
+        if jsonObj is None: return NoProgrammesFound(oc, title)
+    except:
+        return NoProgrammesFound(oc, title)
 
     episodes = jsonObj["episodes"]
   
@@ -265,20 +234,23 @@ def JSONEpisodeListContainer(title, url):
         foundSubtitle = displayTitles["subtitle"]
         pid = thisProgramme["pid"]
         short_synopsis = thisProgramme["short_synopsis"]
+        
+        #TODO: Hardcoded URL's(won't work for BBC HD???)
+        url = config.BBC_SD_PLAYER_URL % pid
+        thumb = config.BBC_SD_THUMB_URL % pid
 
-        oc.add(EpisodeObject(url = config.BBC_SD_PLAYER_URL % pid, title = "%s - %s" % (title, foundSubtitle), summary = short_synopsis, thumb = config.BBC_SD_THUMB_URL % pid))
+        oc.add(EpisodeObject(url = url, title = "%s - %s" % (title, foundSubtitle), summary = short_synopsis, thumb = thumb))
 
     if len(oc) == 0:
-        oc.header  = title
-        oc.message = "No programmes found."
-        return oc
+        return NoProgrammesFound(oc, title)
 
     oc.objects.sort(key=lambda obj: obj.title)
 
     return oc
 
 ##########################################################################################
-def JSONScheduleListContainer(title="", url=None):
+@route(PREFIX + "/VideosFromJSONScheduleList")
+def VideosFromJSONScheduleList(title="", url=None):
     # this function generates the schedule lists for today / yesterday etc. from a JSON feed
     jsonObj = JSON.ObjectFromURL(url)
     if jsonObj is None: return
@@ -320,7 +292,8 @@ def JSONScheduleListContainer(title="", url=None):
     return oc
 
 ##########################################################################################
-def Search(query, search_url = BBC_SEARCH_URL, page_num = 1):
+@route(PREFIX + "/Search")
+def Search(query, search_url, page_num = 1):
     oc = ObjectContainer(title1 = query)
     
     searchResults = HTTP.Request(search_url % (String.Quote(query),page_num)).content
@@ -354,11 +327,16 @@ def Search(query, search_url = BBC_SEARCH_URL, page_num = 1):
                     oc.add(EpisodeObject(url=url, title=title, summary=short_synopsis, duration=duration, originally_available_at=broadcast_date, thumb=config.BBC_SD_THUMB_URL % pid)) 
 
     if len(oc) == 0:
-        oc.header  = query
-        oc.message = "No programmes found."
+        return NoProgrammesFound(oc, query)
     else:
         # See if we need a next button.
         if RE_SEARCH_NEXT.search(searchResults):
             oc.add(NextPageObject(key=Callback(Search, query = query, search_url = search_url, page_num = page_num + 1), title='More...'))
 
+    return oc
+    
+##########################################################################################
+def NoProgrammesFound(oc, title):
+    oc.header  = title
+    oc.message = "No programmes found."
     return oc
--- Contents/Code/content.py
+++ Contents/Code/content.py
@@ -1,7 +1,8 @@
 from config import BBC_HD_PLAYER_URL, BBC_HD_THUMB_URL
 
 class Channel(object):
-    def __init__(self, title, thumb, channel_id, region_id, live_id):
+    def __init__(self, tv, title, thumb, channel_id, region_id, live_id):
+        self.tv = tv
         self.title = title
         self.thumb = thumb
         self.channel_id = channel_id
@@ -11,7 +12,11 @@ class Channel(object):
         if region_id:
             self.schedule_url = self.schedule_url + region_id + "/"
 
-        thumb_url  = """http://www.bbc.co.uk/iplayer/img/tv/%s.jpg"""
+        if tv:
+            thumb_url = """http://www.bbc.co.uk/iplayer/img/tv/%s.jpg"""
+        else:
+            thumb_url = """http://www.bbc.co.uk/iplayer/img/radio/%s.gif"""
+            
         self.thumb_url = thumb_url % self.thumb
 
     def has_highlights(self):
@@ -24,26 +29,46 @@ class Channel(object):
     def popular_url(self):
         return "http://feeds.bbc.co.uk/iplayer/%s/popular" % self.channel_id
 
-    def can_watch_live(self):
+    def has_live_broadcasts(self):
         return self.live_id != None
-
+        
     def live_url(self):
-        return "http://www.bbc.co.uk/iplayer/tv/%s/watchlive" % self.live_id
+        if self.tv:
+            return "http://www.bbc.co.uk/iplayer/tv/%s/watchlive" % self.live_id
+        else:
+            return "http://www.bbc.co.uk/iplayer/radio/%s/listenlive" % self.live_id
 
 tv_channels = {
-    #                           title                thumb               channel_id    region_id  live_id
-    'bbcone':           Channel('BBC One',           'bbc_one',          'bbcone',    'london',  'bbc_one_london'),
-    'bbctwo':           Channel('BBC Two',           'bbc_two',          'bbctwo',    'england', 'bbc_two_england'),
-    'bbcthree':         Channel('BBC Three',         'bbc_three',        'bbcthree',   None,     'bbc_three'),
-    'bbcfour':          Channel('BBC Four',          'bbc_four',         'bbcfour',    None,     'bbc_four'),
-    'cbbc':             Channel('CBBC',              'cbbc',             'cbbc',       None,     'cbbc'),
-    'cbeebies':         Channel('CBeebies',          'cbeebies_1',       'cbeebies',   None,     'cbeebies'),
-    'bbcnews':          Channel('BBC News Channel',  'bbc_news24',       'bbcnews',    None,     'bbc_news24'),
-    'parliament':       Channel('BBC Parliament',    'bbc_parliament_1', 'parliament', None,     'bbc_parliament'),
-    'bbchd':            Channel('BBC HD',            'bbc_hd_1',         'bbchd',      None,     None),
-    'bbcalba':          Channel('BBC Alba',          'bbc_alba',         'bbcalba',    None,     'bbc_alba')
+    #                           Tv       title                thumb               channel_id    region_id  live_id
+    'bbcone':           Channel(True,    'BBC One',           'bbc_one',          'bbcone',    'london',  'bbc_one_london'),
+    'bbctwo':           Channel(True,    'BBC Two',           'bbc_two',          'bbctwo',    'england', 'bbc_two_england'),
+    'bbcthree':         Channel(True,    'BBC Three',         'bbc_three',        'bbcthree',   None,     'bbc_three'),
+    'bbcfour':          Channel(True,    'BBC Four',          'bbc_four',         'bbcfour',    None,     'bbc_four'),
+    'cbbc':             Channel(True,    'CBBC',              'cbbc',             'cbbc',       None,     'cbbc'),
+    'cbeebies':         Channel(True,    'CBeebies',          'cbeebies_1',       'cbeebies',   None,     'cbeebies'),
+    'bbcnews':          Channel(True,    'BBC News Channel',  'bbc_news24',       'bbcnews',    None,     'bbc_news24'),
+    'parliament':       Channel(True,    'BBC Parliament',    'bbc_parliament_1', 'parliament', None,     'bbc_parliament'),
+    'bbchd':            Channel(True,    'BBC HD',            'bbc_hd_1',         'bbchd',      None,     None),
+    'bbcalba':          Channel(True,    'BBC Alba',          'bbc_alba',         'bbcalba',    None,     'bbc_alba')
 }
 ordered_tv_channels = ['bbcone', 'bbctwo', 'bbcthree', 'bbcfour', 'cbbc', 'cbeebies', 'bbcnews', 'parliament', 'bbchd', 'bbcalba']
+
+radio_channels = {
+    #
+    'radio1':           Channel(False,   'BBC Radio 1',                    'bbc_radio_one',                     'radio1',            'england', 'bbc_radio_one'),     
+    '1xtra':            Channel(False,   'BBC 1Xtra',                      'bbc_1xtra',                         '1xtra',             None,      'bbc_1xtra'),
+    'radio2':           Channel(False,   'BBC Radio 2',                    'bbc_radio_two',                     'radio2',            None,      'bbc_radio_two'),
+    'radio3':           Channel(False,   'BBC Radio 3',                    'bbc_radio_three',                   'radio3',            None,      'bbc_radio_three'),
+    'radio4':           Channel(False,   'BBC Radio 4',                    'bbc_radio_four',                    'radio4',            'fm',      'bbc_radio_fourfm'),
+    'radio4extra':      Channel(False,   'BBC Radio 4 Extra',              'bbc_radio_four',                    'radio4extra',       None,      'bbc_radio_four_extra'),
+    '5live':            Channel(False,   'BBC Radio 5 live',               'bbc_radio_five_live',               '5live',             None,      'bbc_radio_five_live'),
+    '5livesportsextra': Channel(False,   'BBC Radio 5 live sports extra',  'bbc_radio_five_live_sports_extra',  '5livesportsextra',  None,      'bbc_radio_five_live_sports_extra'),
+    '6music':           Channel(False,   'BBC 6 Music',                    'bbc_6music',                        '6music',            None,      'bbc_6music'),
+    'asiannetwork':     Channel(False,   'BBC Asian Network',              'bbc_asian_network',                 'asiannetwork',      None,      'bbc_asian_network'),
+    'worldservice':     Channel(False,   'BBC World Service',              'bbc_world_service',                 'worldservice',      None,      'bbc_world_service')
+}
+
+ordered_radio_channels = ['radio1', '1xtra', 'radio2', 'radio3', 'radio4', 'radio4extra', '5live', '5livesportsextra', '6music', 'asiannetwork', 'worldservice']
 
 def slugify(string):
     slug = string.lower()
@@ -60,14 +85,23 @@ class Category(object):
         self.subcategories = [Category(subtitle, []) for subtitle in subcategories]
         self.subcategory = dict([(category.id, category) for category in self.subcategories])
 
-    def popular_url(self):
-        return "http://feeds.bbc.co.uk/iplayer/popular/%s/tv" % self.id
-
-    def highlights_url(self):
-        return "http://feeds.bbc.co.uk/iplayer/highlights/%s/tv" % self.id
+    def popular_url(self, tv):
+        if tv:
+            return "http://feeds.bbc.co.uk/iplayer/popular/%s/tv" % self.id
+        else:
+            return "http://feeds.bbc.co.uk/iplayer/popular/%s/radio" % self.id
+            
+    def highlights_url(self, tv):
+        if tv:
+            return "http://feeds.bbc.co.uk/iplayer/highlights/%s/tv" % self.id
+        else:
+            return "http://feeds.bbc.co.uk/iplayer/highlights/%s/radio" % self.id
 
     def subcategory_url(self, subcategory_id):
         return "http://feeds.bbc.co.uk/iplayer/%s/%s/list" % (self.id, subcategory_id)
+        
+    def genre_url(self, channel_id):            
+        return "http://www.bbc.co.uk/%s/genres/%s/player/episodes.json" % (channel_id, self.id)
 
 categories = [
     Category("Children's", ["Animation", "Drama", "Entertainment & Comedy", "Factual", "Games & Quizzes", "Music", "Other"]),
@@ -86,8 +120,11 @@ class Format(object):
         self.title = title
         self.id = slugify(title)
 
-    def url(self):
-        return "http://www.bbc.co.uk/programmes/formats/%s/player/episodes.json" % self.id
+    def url(self, channel_id=None):
+        if channel_id:
+            return "http://www.bbc.co.uk/%s/programmes/formats/%s/player/episodes.json" % (channel_id, self.id)
+        else:
+            return "http://www.bbc.co.uk/programmes/formats/%s/player/episodes.json" % (self.id)
 
 formats = [
     Format("Animation"),
